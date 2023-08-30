e1f81ef755e8a77b882e513517b13de997260de8
juanchycc <jcaserio@fi.uba.ar>
2023-08-30 16:59:28 -0300

    fix

--- client/common/client.go
+++ client/common/client.go
@@ -4,10 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"time"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -42,7 +42,7 @@ func (c *Client) createClientSocket() error {
 	conn, err := net.Dial("tcp", c.config.ServerAddress)
 	if err != nil {
 		log.Fatalf(
-	        "action: connect | result: fail | client_id: %v | error: %v",
+			"action: connect | result: fail | client_id: %v | error: %v",
 			c.config.ID,
 			err,
 		)
@@ -56,24 +56,24 @@ func (c *Client) StartClientLoop() {
 	// autoincremental msgID to identify every message sent
 	msgID := 1
 
-sigchnl := make(chan os.Signal, 1)
-signal.Notify(sigchnl, syscall.SIGTERM)
+	sigchnl := make(chan os.Signal, 1)
+	signal.Notify(sigchnl, syscall.SIGTERM)
 
 loop:
 
 	// Send messages if the loopLapse threshold has not been surpassed
 	for timeout := time.After(c.config.LoopLapse); ; {
 		select {
-		case <-timeout:
-	        log.Infof("action: timeout_detected | result: success | client_id: %v",
-                c.config.ID,
-            )
-			break loop
 		case sig := <-sigchnl:
-			
+
 			log.Infof("action: signal_detected -> %v | result: success | client_id: %v", sig, c.config.ID)
 			// Cierro socket
 			c.conn.Close()
+			break loop
+		case <-timeout:
+			log.Infof("action: timeout_detected | result: success | client_id: %v",
+				c.config.ID,
+			)
 			break loop
 		default:
 		}
@@ -94,19 +94,19 @@ loop:
 
 		if err != nil {
 			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-                c.config.ID,
+				c.config.ID,
 				err,
 			)
 			return
 		}
 		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-            c.config.ID,
-            msg,
-        )
+			c.config.ID,
+			msg,
+		)
 
 		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-		
+		//time.Sleep(c.config.LoopPeriod)
+
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
